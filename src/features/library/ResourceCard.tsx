110111569d2747f76b443adbe0df923afe53e173
Asadbek Azamjonov <aaazamjohn@gmail.com>
2026-08-01 08:00:56 +0200

    feat: improve resource card experience

--- src/features/library/ResourceCard.tsx
+++ src/features/library/ResourceCard.tsx
@@ -1,15 +1,18 @@
-import type { Resource } from "../../types/resource";
 import { Link } from "react-router-dom";
+import type { Resource } from "../../types/resource";
 import { FavoriteButton } from "./FavoriteButton";
 
 export function ResourceCard({ resource }: { resource: Resource }) {
   return (
-    <article>
-      <h3>{resource.title}</h3>
-      <p>{resource.category}</p>
+    <article className="resource-card">
       <span>{resource.type}</span>
-      <FavoriteButton id={resource.id} />
-      <Link to={`/viewer/${resource.id}`}>Open</Link>
+      <h3>{resource.title}</h3>
+      <p>{resource.description ?? "Learning resource"}</p>
+      <small>{resource.category}</small>
+      <div>
+        <FavoriteButton id={resource.id} />
+        <Link to={`/viewer/${resource.id}`}>Open</Link>
+      </div>
     </article>
   );
 }
