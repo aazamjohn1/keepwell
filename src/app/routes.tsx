778651c4050f2fee554e80975bfc65b7ca795c19
Asadbek Azamjonov <aaazamjohn@gmail.com>
2026-08-01 07:52:52 +0200

    feat: add layout routing

--- src/app/routes.tsx
+++ src/app/routes.tsx
@@ -4,15 +4,18 @@ import { LibraryPage } from "../pages/LibraryPage";
 import { SearchPage } from "../pages/SearchPage";
 import { FavoritesPage } from "../pages/FavoritesPage";
 import { Collections } from "../features/collections/Collections";
+import { AppLayout } from "../layouts/AppLayout";
 
 export function AppRoutes() {
   return (
     <Routes>
-      <Route path="/" element={<Dashboard />} />
-      <Route path="/library" element={<LibraryPage />} />
-      <Route path="/search" element={<SearchPage />} />
-      <Route path="/favorites" element={<FavoritesPage />} />
-      <Route path="/collections" element={<Collections />} />
+      <Route element={<AppLayout />}>
+        <Route path="/" element={<Dashboard />} />
+        <Route path="/library" element={<LibraryPage />} />
+        <Route path="/search" element={<SearchPage />} />
+        <Route path="/favorites" element={<FavoritesPage />} />
+        <Route path="/collections" element={<Collections />} />
+      </Route>
     </Routes>
   );
 }
