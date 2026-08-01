b691471bf95409cf415f068465b1cb622231b1b0
Asadbek Azamjonov <aaazamjohn@gmail.com>
2026-08-01 07:43:47 +0200

    feat: track viewed resources in viewer

--- src/pages/ViewerPage.tsx
+++ src/pages/ViewerPage.tsx
@@ -1,10 +1,18 @@
+import { useEffect } from "react";
 import { useParams } from "react-router-dom";
 import { ResourceViewer } from "../features/viewer/ResourceViewer";
 import { useResourceById } from "../features/viewer/useResourceById";
+import { trackViewed } from "../features/viewer/trackViewed";
 
 export function ViewerPage() {
   const { id } = useParams();
   const resource = useResourceById(id ?? "");
+
+  useEffect(() => {
+    if (resource) {
+      trackViewed(resource.id);
+    }
+  }, [resource]);
 
   if (!resource) {
     return <p>Resource not found.</p>;
